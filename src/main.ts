f5d9f82ce6b06154624d7e83dc50c07745921844
martinc <martin.developer25@gmail.com>
2024-07-02 14:53:06 -0300

    cambio de prefijo

--- src/main.ts
+++ src/main.ts
@@ -3,23 +3,26 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AuthGuard } from './common/guard/guard';
 import * as dotenv from 'dotenv'
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const globalPrefix = 'arvent';
   dotenv.config()
 
   const config = new DocumentBuilder()
     .setTitle('Chronos Prestamos')
     .setDescription('The chronos prestamos API description')
     .setVersion('1.0')
-    .addTag('api')
+    .addTag(globalPrefix)
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
-  const globalPrefix = 'arvent';
+  SwaggerModule.setup(globalPrefix, app, document);
   app.setGlobalPrefix(globalPrefix);
   app.useGlobalGuards(new AuthGuard());
 
-  await app.listen(3000);
+  await app.listen(3000, () => {
+    Logger.log(' Listening at  http://localhost:' + 3000 + '/' + globalPrefix);
+  });
 }
 bootstrap();
