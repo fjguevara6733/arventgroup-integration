59825f6e6b4b30003a1501a5794e7a1ca1f830c0
martinc <martin.developer25@gmail.com>
2025-05-14 13:18:38 -0300

    agregar conexion a arvent dev

--- src/database.module.ts
+++ src/database.module.ts
@@ -4,43 +4,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: () => {
-        const host = 'localhost';
-        const port = 5432;
-        const username = 'postgres';
-        const password = '1234';
-        const database = 'arvent_group_dev';
-
-        return {
-          name: 'arventGroup',
-          type: 'postgres',
-          host,
-          port,
-          username,
-          password,
-          database,
-          entities: [__dirname + '/**/*.entity{.ts,.js}'],
-          synchronize: false,
-          logging: ['error'],
-        };
-      },
-    }),
     // TypeOrmModule.forRootAsync({
     //   imports: [ConfigModule],
     //   inject: [ConfigService],
     //   useFactory: () => {
-    //     const host = 'database-chronospay-prod.cz6kkw664bxb.us-east-1.rds.amazonaws.com';
-    //     const port = 3306;
-    //     const username = 'chronoslive';
-    //     const password = 'h}z4fh92VBKu*xV<';
-    //     const database = 'arvent_group';
+    //     const host = 'localhost';
+    //     const port = 5432;
+    //     const username = 'postgres';
+    //     const password = '1234';
+    //     const database = 'arvent_group_dev';
 
     //     return {
     //       name: 'arventGroup',
-    //       type: 'mysql',
+    //       type: 'postgres',
     //       host,
     //       port,
     //       username,
@@ -51,7 +27,31 @@ import { TypeOrmModule } from '@nestjs/typeorm';
     //       logging: ['error'],
     //     };
     //   },
-    // })
+    // }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: () => {
+        const host = 'database-chronospay-prod.cz6kkw664bxb.us-east-1.rds.amazonaws.com';
+        const port = 3306;
+        const username = 'chronostest';
+        const password = ',h1e6#STJBqZ9sSj';
+        const database = 'arvent_group_dev';
+
+        return {
+          name: 'arventGroup',
+          type: 'mysql',
+          host,
+          port,
+          username,
+          password,
+          database,
+          entities: [__dirname + '/**/*.entity{.ts,.js}'],
+          synchronize: false,
+          logging: ['error'],
+        };
+      },
+    })
   ],
   exports: [TypeOrmModule],
 })
